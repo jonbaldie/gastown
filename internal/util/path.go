6182949b293b3add8edd6bcfd6b272a2b14c503b
Jonathan Baldie <jon@jonbaldie.com>
2026-08-28 12:52:16 +0100

    refactor: remove home directory cache state

--- internal/util/path.go
+++ internal/util/path.go
@@ -3,21 +3,7 @@ package util
 import (
 	"os"
 	"strings"
-	"sync"
 )
-
-var (
-	homeDir     string
-	homeDirOnce sync.Once
-)
-
-// cachedHomeDir returns the user's home directory, cached after the first call.
-func cachedHomeDir() string {
-	homeDirOnce.Do(func() {
-		homeDir, _ = os.UserHomeDir()
-	})
-	return homeDir
-}
 
 // ExpandHome expands a leading ~/ to the user's home directory.
 // Returns the path unchanged if it doesn't start with ~/ or if
@@ -26,7 +12,7 @@ func ExpandHome(path string) string {
 	if !strings.HasPrefix(path, "~/") {
 		return path
 	}
-	home := cachedHomeDir()
+	home, _ := os.UserHomeDir()
 	if home == "" {
 		return path
 	}
