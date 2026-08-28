a1748aaa101c0ddc5eb4e10493b25e56e8f55102
Jonathan Baldie <jon@jonbaldie.com>
2026-08-28 22:21:13 +0100

    refactor(rig): split tracked beads import inspection

--- internal/rig/tracked_import.go
+++ internal/rig/tracked_import.go
@@ -31,85 +31,133 @@ func (imp TrackedBeadsImport) RequiresConsent() bool {
 // hooks under a cloned Rig source root (typically mayor/rig).
 func InspectTrackedBeadsImport(sourceRoot string) (TrackedBeadsImport, error) {
 	imp := TrackedBeadsImport{Source: sourceRoot}
-	beadsDir := filepath.Join(sourceRoot, ".beads")
-	info, err := os.Lstat(beadsDir)
-	if err != nil && !os.IsNotExist(err) {
+	if err := inspectTrackedBeadsDir(sourceRoot, &imp); err != nil {
 		return TrackedBeadsImport{}, err
 	}
-	if err == nil && info.Mode()&os.ModeSymlink != 0 {
-		return TrackedBeadsImport{}, fmt.Errorf("refusing symlinked Beads directory %s", beadsDir)
+	if err := inspectTrackedHooks(sourceRoot, &imp); err != nil {
+		return TrackedBeadsImport{}, err
 	}
-	if err == nil && info.IsDir() {
-		if err := filepath.WalkDir(beadsDir, func(path string, d fs.DirEntry, walkErr error) error {
-			if walkErr != nil {
-				return walkErr
-			}
-			if d.Type()&os.ModeSymlink != 0 {
-				return fmt.Errorf("refusing symlinked tracked Beads path %s", path)
-			}
-			if d.IsDir() {
-				return nil
-			}
-			if strings.HasSuffix(d.Name(), ".jsonl") {
-				count, err := countJSONLRecords(path)
-				if err != nil {
-					return err
-				}
-				if count > 0 {
-					rel, _ := filepath.Rel(sourceRoot, path)
-					imp.JSONLFiles = append(imp.JSONLFiles, filepath.ToSlash(rel))
-					imp.BeadCount += count
-				}
-			}
-			if isExecutableHook(path, d) {
-				rel, _ := filepath.Rel(sourceRoot, path)
-				imp.ExecutableHooks = append(imp.ExecutableHooks, filepath.ToSlash(rel))
-			}
+	return imp, nil
+}
+
+func inspectTrackedBeadsDir(sourceRoot string, imp *TrackedBeadsImport) error {
+	beadsDir := filepath.Join(sourceRoot, ".beads")
+	info, err := os.Lstat(beadsDir)
+	if err != nil {
+		if os.IsNotExist(err) {
 			return nil
-		}); err != nil {
-			return TrackedBeadsImport{}, err
+		}
+		return err
+	}
+	if info.Mode()&os.ModeSymlink != 0 {
+		return fmt.Errorf("refusing symlinked Beads directory %s", beadsDir)
+	}
+	if !info.IsDir() {
+		return nil
+	}
+	return filepath.WalkDir(beadsDir, func(path string, d fs.DirEntry, walkErr error) error {
+		return inspectTrackedBeadsPath(sourceRoot, imp, path, d, walkErr)
+	})
+}
+
+func inspectTrackedBeadsPath(sourceRoot string, imp *TrackedBeadsImport, path string, d fs.DirEntry, walkErr error) error {
+	if walkErr != nil {
+		return walkErr
+	}
+	if d.Type()&os.ModeSymlink != 0 {
+		return fmt.Errorf("refusing symlinked tracked Beads path %s", path)
+	}
+	if d.IsDir() {
+		return nil
+	}
+	if strings.HasSuffix(d.Name(), ".jsonl") {
+		if err := recordTrackedJSONL(sourceRoot, imp, path); err != nil {
+			return err
 		}
 	}
+	recordTrackedHook(sourceRoot, imp, path, d)
+	return nil
+}
 
+func recordTrackedJSONL(sourceRoot string, imp *TrackedBeadsImport, path string) error {
+	count, err := countJSONLRecords(path)
+	if err != nil {
+		return err
+	}
+	if count == 0 {
+		return nil
+	}
+	rel, _ := filepath.Rel(sourceRoot, path)
+	imp.JSONLFiles = append(imp.JSONLFiles, filepath.ToSlash(rel))
+	imp.BeadCount += count
+	return nil
+}
+
+func recordTrackedHook(sourceRoot string, imp *TrackedBeadsImport, path string, d fs.DirEntry) {
+	if !isExecutableHook(path, d) {
+		return
+	}
+	rel, _ := filepath.Rel(sourceRoot, path)
+	imp.ExecutableHooks = append(imp.ExecutableHooks, filepath.ToSlash(rel))
+}
+
+func inspectTrackedHooks(sourceRoot string, imp *TrackedBeadsImport) error {
 	for _, hooksDir := range []string{
 		filepath.Join(sourceRoot, ".git", "hooks"),
 		filepath.Join(sourceRoot, ".beads", "hooks"),
 	} {
-		info, err := os.Lstat(hooksDir)
-		if err != nil {
-			if os.IsNotExist(err) {
-				continue
-			}
-			return TrackedBeadsImport{}, err
-		}
-		if info.Mode()&os.ModeSymlink != 0 {
-			return TrackedBeadsImport{}, fmt.Errorf("refusing symlinked hooks directory %s", hooksDir)
-		}
-		entries, err := os.ReadDir(hooksDir)
-		if err != nil {
-			return TrackedBeadsImport{}, err
-		}
-		for _, entry := range entries {
-			if entry.IsDir() {
-				continue
-			}
-			if entry.Type()&os.ModeSymlink != 0 {
-				return TrackedBeadsImport{}, fmt.Errorf("refusing symlinked hook %s", filepath.Join(hooksDir, entry.Name()))
-			}
-			path := filepath.Join(hooksDir, entry.Name())
-			info, err := entry.Info()
-			if err != nil {
-				continue
-			}
-			if isExecutableFile(path, info) && !strings.HasSuffix(entry.Name(), ".sample") {
-				rel, _ := filepath.Rel(sourceRoot, path)
-				if !containsString(imp.ExecutableHooks, filepath.ToSlash(rel)) {
-					imp.ExecutableHooks = append(imp.ExecutableHooks, filepath.ToSlash(rel))
-				}
-			}
+		if err := inspectTrackedHooksDir(sourceRoot, hooksDir, imp); err != nil {
+			return err
 		}
 	}
-	return imp, nil
+	return nil
+}
+
+func inspectTrackedHooksDir(sourceRoot, hooksDir string, imp *TrackedBeadsImport) error {
+	info, err := os.Lstat(hooksDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return err
+	}
+	if info.Mode()&os.ModeSymlink != 0 {
+		return fmt.Errorf("refusing symlinked hooks directory %s", hooksDir)
+	}
+	entries, err := os.ReadDir(hooksDir)
+	if err != nil {
+		return err
+	}
+	for _, entry := range entries {
+		if err := inspectTrackedHookEntry(sourceRoot, hooksDir, imp, entry); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func inspectTrackedHookEntry(sourceRoot, hooksDir string, imp *TrackedBeadsImport, entry os.DirEntry) error {
+	if entry.IsDir() {
+		return nil
+	}
+	path := filepath.Join(hooksDir, entry.Name())
+	if entry.Type()&os.ModeSymlink != 0 {
+		return fmt.Errorf("refusing symlinked hook %s", path)
+	}
+	info, err := entry.Info()
+	if err != nil {
+		return nil
+	}
+	if strings.HasSuffix(entry.Name(), ".sample") || !isExecutableFile(path, info) {
+		return nil
+	}
+	rel, _ := filepath.Rel(sourceRoot, path)
+	relPath := filepath.ToSlash(rel)
+	if containsString(imp.ExecutableHooks, relPath) {
+		return nil
+	}
+	imp.ExecutableHooks = append(imp.ExecutableHooks, relPath)
+	return nil
 }
 
 // RequireTrackedBeadsImportConsent rejects silent activation of tracked Beads
